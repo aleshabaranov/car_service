94ad9431810244e7bc13c6a54b7352d4833c9cb0
aleshabaranov <61497343+aleshabaranov@users.noreply.github.com>
2020-04-23 19:20:14 +0500

    Update common.js

--- libs/common.js
+++ libs/common.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 		}
 		$.ajax({
 			type: "POST",
-			url: "mail.php",
+			url: " ",
 			data: $(this).serialize()
 		}).done(function() {
 			$('.js-overlay-thank-you').fadeIn();
